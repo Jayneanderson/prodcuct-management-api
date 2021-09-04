faef2370290274a897d2cb5036adb4df5a755f41
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-09-03 22:32:03 -0300

    Adição: aqui foi acrescentada a classe responsável por pegar a URL de upload da imagem

---
+++ src/main/java/com/threestyle/productapi/controller/upload/UploadOutput.java
@@ -0,0 +1,14 @@
+package com.threestyle.productapi.controller.upload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadOutput {
+	
+	private String url;
+
+}
