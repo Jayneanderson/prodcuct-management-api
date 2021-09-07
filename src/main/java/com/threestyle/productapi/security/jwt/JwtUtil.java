ae6cb61d0e4991faada591824c66949df67f2694
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-09-07 19:28:48 -0300

    Atualização: os métodos de criação, validação, expiração do token, entre outros fora desenvolvidos

--- src/main/java/com/threestyle/productapi/security/jwt/JwtUtil.java
+++ src/main/java/com/threestyle/productapi/security/jwt/JwtUtil.java
@@ -1,37 +1,47 @@
 package com.threestyle.productapi.security.jwt;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.Keys;;
 
 //esta classe utiliza a lib JSON Web Token
 public class JwtUtil {
 	/*
 	 * Esta classe tem métodos para gerar, validar um token
 	 */
-	
-	/* Esta chave foi gerada com algoritmo HS512 nesse site:
+
+	/*
+	 * Esta chave foi gerada com algoritmo HS512 nesse site:
 	 * https://www.allkeysgenerator.com/
 	 */
 	public static final String JWT_SECRET = "!A%D*G-KaPdSgVkYp2s5v8y/B?E(H+MbQeThWmZq4t6w9z$C&F)J@NcRfUjXn2r5";
-	
-	//método para pegar as credenciais
+
+	// método para pegar as credenciais
 	public static Claims getClaims(String token) {
 		byte[] bytesSigningKey = getSigningKey();
-		
-		//pego a chave que foi gerada e faço um parse
-		
-		return Jwts.parserBuilder()
-				.setSigningKey(bytesSigningKey)
-				.
+		token = token.replace("Bearer", "");
+
+		return Jwts.parser()// pego a chave que foi gerada e faço um parse
+				.setSigningKey(bytesSigningKey).parseClaimsJws(token).getBody();
+	}
+
+	public static String getLogin(String token) {
+		Claims claims = getClaims(token);
+		return !isNull(claims) ? claims.getSubject() : null;
 	}
 
 	// método para converter dias em milisegundos
@@ -41,13 +51,35 @@ public class JwtUtil {
 				* 60 /* segundos */ * 1000 /* milisegundos */;
 		return new Date(System.currentTimeMillis() + time);
 	}
-	
+
+	public static List<GrantedAuthority> getRoles(String token) {
+		Claims claims = getClaims(token);
+
+		if (claims == null) {
+			return null;
+		}
+		return ((List<?>) claims.get("rol")).stream().map(authority -> new SimpleGrantedAuthority((String) authority))
+				.collect(Collectors.toList());
+
+	}
+
+	public static boolean isTokenValid(String token) {
+		Claims claims = getClaims(token);
+		if (nonNull(claims)) {
+			String login = claims.getSubject();
+			Date expiration = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			return login != null && expiration != null && now.before(expiration);
+		}
+
+		return false;
+	}
+
 	private static byte[] getSigningKey() {
 		return JWT_SECRET.getBytes();
 	}
 
 	public static String createToken(UserDetails user) {
-
 
 		// aqui eu pego a lita de roles
 		List<String> roles = user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
@@ -69,4 +101,20 @@ public class JwtUtil {
 				// agora mando gerar
 				.compact();
 	}
+
+		public static String getAuthLogin() {
+			UserDetails user = getUserDetails();
+			if (user != null) {
+				return user.getUsername();
+			}
+			return null;
+		}
+	
+		public static UserDetails getUserDetails() {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			if (auth != null && auth.getPrincipal() != null) {
+				return (UserDetails) auth.getPrincipal();
+			}
+			return null;
+		}
 }
